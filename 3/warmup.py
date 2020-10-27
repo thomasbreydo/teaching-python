fe4de591ace6b0cb89fc6481fbec29ebbed2d121
Thomas Breydo <tbreydo@gmail.com>
2020-10-27 12:42:10 -0400

    Add warmup for lesson 3

---
+++ 3/warmup.py
@@ -0,0 +1,22 @@
+"""
+Write a program that tells a student what they should depending on if it's the weekend:
+
+1.
+  Ask, "Is it the weekend (yes or no)? "
+
+2.
+  If it's the weekend, use 3 print statements to say:
+    sleep
+    brunch
+    chill
+
+  If it's a weekday, use 2 print statements to say:
+    wake up
+    go to school
+
+*3.
+  If the user says something other than "yes" or "no", print with "Invalid input"
+
+*4.
+  Treat inputs like "Yes" and "YES" as valid, and print responses correctly.
+"""
