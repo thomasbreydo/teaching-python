83baed7eecf9251de1d97c6b285662a426ab07a5
Thomas Breydo <tbreydo@gmail.com>
2020-11-03 17:39:13 -0500

    Done w class 4

---
+++ 4/functions.py
@@ -0,0 +1,36 @@
+# def greet_thomas():
+#     print("Hello, thomas")
+#
+# def greet_max():
+#     print("Hello, max")
+#
+# def greet_tyler():
+#     print("Hello, tyler")
+#
+#
+# greet_max()
+# greet_tyler()
+# greet_thomas()
+
+
+def f(x):
+    print("Hello " + x)
+
+f("max")
+
+
+
+# double_the_number(30)
+# Double that number is 60
+
+# f(x) = 2x
+# f(500)
+
+
+# def func():
+#     print("Indent with tab")
+#     print("Lines don't run at first")
+#
+#
+# func()
+# func()
---
+++ 4/warmup.py
@@ -0,0 +1,37 @@
+"""
+ 1. Write a program that asks the user for the length and width of a
+    rectangle and prints the area of the rectangle.
+
+
+    (Bonus: print the perimeter.)
+
+
+ 2. Hard. (I haven't fully covered this yet, so *skip it* if you want.)
+    Turn the area calculation above into a function. Define a function
+    called area() that takes in a width and height and returns their product.
+
+    IF YOU FINISH this problem, send me a message on Zoom.
+
+ 3. Write a program that asks the user for their age and tells them if they
+    can vote (18 or older)
+"""
+
+# f(x) = 2x
+# f(x, y) = 2x + 5y
+
+def area(width, length):
+    return width * length
+
+x = area(5, 6)
+
+print(x)
+
+def perimeter(width, length):
+    # return the perimeter
+
+
+#
+# length = int(input("Length? "))
+# width = int(input("Width? "))
+#
+# print(length * width)
